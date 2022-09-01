1575e3d1ea6020a8765cc5f867a3e16e27d0602d
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-09-01 16:32:07 +0800

    add pre loading

--- js/main.js
+++ js/main.js
@@ -5,17 +5,10 @@ let body = document.querySelector('#body')
 let nav = document.querySelector('.main-header')
 let leftOrientation = document.querySelector('.left-orientation')
 let rightOrientation = document.querySelector('.right-orientation')
-
+let preloader = document.querySelector('#preloader')
 let prevScrollpos = window.scrollY;
 
-window.addEventListener('resize', function() {
-    if (window.innerWidth > 768 && navbar.classList.contains('open-menu')) {
-        navbar.classList.remove('open-menu')
-        content.classList.remove('active-blur')
-        body.style.overflow = 'visible'
-        menu.classList.toggle('move')
-    }
-
+const checkWidth = () => {
     if (window.innerWidth <= 768) {
         leftOrientation.style.display = 'none'
         rightOrientation.style.display = 'none'
@@ -23,6 +16,23 @@ window.addEventListener('resize', function() {
         leftOrientation.style.display = 'inline'
         rightOrientation.style.display = 'inline'
     }
+}
+
+window.onload = () => {
+    checkWidth()
+
+    preloader.style.display = 'none'
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768 && navbar.classList.contains('open-menu')) {
+        navbar.classList.remove('open-menu')
+        content.classList.remove('active-blur')
+        body.style.overflow = 'visible'
+        menu.classList.toggle('move')
+    }
+
+    checkWidth()
 });
 
 menu.onclick = () => {
