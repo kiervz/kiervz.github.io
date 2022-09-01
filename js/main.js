c88e8951943a157774b2c4291515133464661fb5
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-09-01 14:22:16 +0800

    done responsive navigation bar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-$(document).ready(function() {
-    $('.nav-link').on('click', function(){  
-        $('a').removeClass('active');  
-        $(this).addClass('active');
-    });
+let menu = document.querySelector('.menu-icon')
+let navbar = document.querySelector('.navbar')
+let content = document.querySelector('#content')
+let body = document.querySelector('#body')
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 768 && navbar.classList.contains('open-menu')) {
+        navbar.classList.remove('open-menu')
+        content.classList.remove('active-blur')
+        body.style.overflow = 'visible'
+        menu.classList.toggle('move')
+    }
 });
+
+menu.onclick = () => {
+    if (!navbar.classList.contains('open-menu')) {
+        navbar.classList.add('open-menu')
+        navbar.classList.remove('close-menu')
+        content.classList.add('active-blur')
+        body.style.overflow = 'hidden'
+    } else {
+        navbar.classList.add('close-menu')
+        navbar.classList.remove('open-menu')
+        content.classList.remove('active-blur')
+        body.style.overflow = 'visible'
+    }
+    menu.classList.toggle('move')
+}
