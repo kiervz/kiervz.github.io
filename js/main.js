0cb739b5965a638e637ed5ef910b8864c3e86b8d
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-09-21 12:18:41 +0800

    fix navigation link active

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 let menu = document.querySelector('.menu-icon')
 let mainHeader = document.querySelector('.main-header')
 let navbar = document.querySelector('.navbar')
-let navLink = document.querySelector(".nav-link")
+let navLink = document.querySelectorAll('.nav-link')
 let content = document.querySelector('#content')
 let body = document.querySelector('#body')
 let nav = document.querySelector('.main-header')
 let leftOrientation = document.querySelector('.left-orientation')
 let rightOrientation = document.querySelector('.right-orientation')
 let preloader = document.querySelector('#preloader')
+let sections = document.querySelectorAll('section[id]')
 
 let prevScrollpos = window.scrollY;
 
@@ -24,18 +25,25 @@ const checkWidth = () => {
 window.onload = () => {
     checkWidth()
 
-    setTimeout(() => {
-        preloader.style.display = 'none'
-    }, 2000)
+    // setTimeout(() => {
+    //     preloader.style.display = 'none'
+    // }, 2000)
 }
 
-navLink.addEventListener('click', function() {
+const navLinkAction = (e) => {
+    navbar.classList.add('close-menu')
     navbar.classList.add('close-menu')
     navbar.classList.remove('open-menu')
     content.classList.remove('active-blur')
     menu.classList.remove('move')
     body.style.overflow = 'visible'
-});
+    
+
+    navLink.forEach(n => n.classList.remove('active-link'))
+    document.querySelector(`#${e.target.id}`).classList.add('active-link')
+}
+
+navLink.forEach(n => n.addEventListener('click', navLinkAction))
 
 window.addEventListener('resize', () => {
     if (window.innerWidth > 768 && navbar.classList.contains('open-menu')) {
@@ -76,3 +84,21 @@ window.onscroll = () => {
 
     if(currentScrollPos >= 100) mainHeader.classList.add('shadow-header'); else mainHeader.classList.remove('shadow-header')
 }
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('#nav-' + sectionId).classList.add('active-link')
+        }else{
+            document.querySelector('#nav-' + sectionId).classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
