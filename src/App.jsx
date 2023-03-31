a3cdb5e2105c7d44afb25c86e87b972de3d7c83f
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-03-31 21:00:44 +0800

    Deploy new portfolio to GitHub Pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import BgImage from './assets/kierveymaghanoy.png';
+
+function App() {
+  return (
+    <>
+     <img 
+      src={BgImage} 
+      className='image' 
+      alt='Best porfolio ever'
+    />
+    </>
+  )
+}
+
+export default App
